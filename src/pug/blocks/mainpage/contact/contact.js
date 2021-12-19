3ab4a55543a8a95e35f029c6354adc62bb8bd426
David <david.shariev.2002@gmail.com>
2021-12-19 13:33:15 +0300

    Added BLOCK CONTACT mainpage

---
+++ src/pug/blocks/mainpage/contact/contact.js
@@ -0,0 +1,5 @@
+console.log("%c BLOCK CONTACT is onloaded", "color: purple; font-size: 20px;");
+
+
+
+import "./contact.scss";
--- src/pug/pages/mainpage/mainpage.js
+++ src/pug/pages/mainpage/mainpage.js
@@ -7,4 +7,5 @@ import "../../blocks/mainpage/portfolio/portfolio.js";
 import "../../blocks/mainpage/packages/packages.js";
 import "../../blocks/mainpage/stories/stories.js";
 import "../../blocks/mainpage/something/something.js";
+import "../../blocks/mainpage/contact/contact.js";
 
